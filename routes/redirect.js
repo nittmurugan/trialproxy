c3c1433292bcc9aeec2d30af5ccb9ffef0c9ab0f
murugan <coder.murugan@gmail.com>
2015-09-09 16:09:30 +0530

    missed changes on previous checkin

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,5 +1,6 @@
 'use strict';
 var express = require('express');
+var querystring = require('querystring');
 var router = express.Router();
 
 /* GET home page. */
@@ -23,10 +24,13 @@ router.get('/', function(req, res, next) {
     throw new Error('name not found');
   }
 
-  var sum = parseInt(req.query.num1) + parseInt(req.query.num2);
+  var sum = { sum: parseInt(req.query.num1) + parseInt(req.query.num2) };
+  var cookyies = querystring.stringify(sum, ';',':');
 
   res.cookie('sum', sum)
-    .redirect('http://localhost:8000/hello?name=susan');
+    .redirect('http://localhost:8000/hello?'
+      + querystring.stringify({ name: 'susan',
+                                cookies: cookyies}));
 });
 
 module.exports = router;
